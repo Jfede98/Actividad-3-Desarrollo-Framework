88ced4e9c83dd4b78ef141b6e78ae34148a1516e
tatianavasquezj <tatianavasquezj@gmail.com>
2025-06-27 00:21:50 -0500

    Actualizacion componente Brand

---
+++ piano-vue/src/components/Brand/ComponentBrand.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg">
+  <p>&nbsp;</p>
+</div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.bg{
+     background-image: url('/src/assets/bg_img.webp');
+     width:100%;
+     height: 6rem;
+     background-attachment: fixed;
+     background-position:top center;
+     background-size: 100% 20%;
+     margin: 0%;
+}
+</style>
