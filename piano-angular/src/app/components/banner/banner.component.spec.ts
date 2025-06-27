796be4e680cad499c2f0d63774e2474c441388da
Isabel Ordóñez <macrina.ordonez@hotmail.com>
2025-06-26 23:16:03 -0500

    Actualizando banner.component.spec.ts

--- piano-angular/src/app/components/banner/banner.component.spec.ts
+++ piano-angular/src/app/components/banner/banner.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BannerComponent } from './banner.component';
+import { CarrousellComponent } from './carrousell.component';
 
-describe('BannerComponent', () => {
-  let component: BannerComponent;
-  let fixture: ComponentFixture<BannerComponent>;
+describe('CarrousellComponent', () => {
+  let component: CarrousellComponent;
+  let fixture: ComponentFixture<CarrousellComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BannerComponent]
+      imports: [CarrousellComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(BannerComponent);
+    fixture = TestBed.createComponent(CarrousellComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
