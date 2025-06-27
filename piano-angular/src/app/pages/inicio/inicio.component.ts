b082b4ed0eb704e2fc2781c9cf669e48066c5541
Julian Riera <juriera98@gmail.com>
2025-06-26 23:31:34 -0500

    Actualizando la pagina inicio

--- piano-angular/src/app/pages/inicio/inicio.component.ts
+++ piano-angular/src/app/pages/inicio/inicio.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { BannerComponent } from '../../components/banner/banner.component';
+import { AsideComponent } from '../../components/aside/aside.component';
+import { VideoComponent } from '../../components/video/video.component';
+import { BrandComponent } from '../../components/brand/brand.component';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    BannerComponent,
+    AsideComponent,
+    VideoComponent,
+    BrandComponent
+  ],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
-
-}
+export class InicioComponent {}
