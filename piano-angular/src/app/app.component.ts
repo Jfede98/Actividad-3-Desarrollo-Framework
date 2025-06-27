1d0922f3d4e991e1ab0448aa61243080697ca074
Julian Riera <juriera98@gmail.com>
2025-06-26 22:10:41 -0500

    Se agregaron todos los componentes que se van a usar en el proyecto. Edicion de archivo app.component.ts

--- piano-angular/src/app/app.component.ts
+++ piano-angular/src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { VideoComponent } from './components/video/video.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    BannerComponent,
+    AsideComponent,
+    VideoComponent,
+    BrandComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
